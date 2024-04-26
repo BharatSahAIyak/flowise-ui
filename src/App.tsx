3831ae8882348f008ef34970b36e5faa9b04d70b
thekavikumar <kavikumarceo@gmail.com>
2024-04-26 18:20:48 +0530

    done implementing flowise

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Admin, CustomRoutes, Resource } from "react-admin";
 import { Route } from "react-router-dom";
-import Sample from "./components/flowise/Sample";
+import Flowise from "./components/flowise/Flowise";
 
 export const App = () => (
   <Admin>
-    <Resource name="" />
+    <Resource name="home" list={<>Hello there</>} />
+    {/* <Resource name="flowise" list={<Flowise />} /> */}
     <CustomRoutes>
-      <Route path="/flowise" element={<Sample />} />
+      <Route path="/flowise" element={<Flowise />} />
     </CustomRoutes>
   </Admin>
 );
